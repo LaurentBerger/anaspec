7bb160579bc1b576db4b447c369c553c6e7b5478
LaurentBerger <laurent.berger@univ-lemans.fr>
2022-12-13 10:35:50 +0100

    update

--- anaspec/divers/mp3_wav.py
+++ anaspec/divers/mp3_wav.py
@@ -2,10 +2,10 @@ import os
 import ffmpeg
 import wx
 
-# check if ffmpeg is available on windows
+# check if ffmpeg is available on windows https://www.gyan.dev/ffmpeg/builds/ffmpeg-git-essentials.7z
 if os.path.splitdrive(os.getcwd())[0] != '':
     if os.path.exists("c:/temp/ffmpeg/bin"):
-        os.environ["PATH"] = "c:/temp/ffmpeg/bin" + os.pathsep + os.environ["PATH"]
+        os.environ["PATH"] = "c:/temp/ffmpeg-git-essentials.7z/bin" + os.pathsep + os.environ["PATH"]
     else:
         wx.LogWarning("ffmpeg not found\nffmpeg must be in system path")
 wx.LogMessage("Convert a wav file to mp3 (8, 32, 128, 320kbps) and mp3 to wav")
@@ -13,7 +13,7 @@ wx.LogMessage("Convert a wav file to mp3 (8, 32, 128, 320kbps) and mp3 to wav")
 my_app = wx.App()
 nom_fichier_ref = wx.FileSelector("Select wav file to convert",wildcard="*.wav")
 del my_app
-audio_wav = ffmpeg.input(nom_fichier_ref + ".wav")
+audio_wav = ffmpeg.input(nom_fichier_ref)
 bit_rate = ['8k', '32k', '128k', '320k']
 for ab in bit_rate: 
     nom_fichier = nom_fichier_ref + "_" + ab + ".mp3"
